0250bb51c4d64ba932ada17b28e76f73969bc152
Maleesha Rodrigo <w1902272@my.westminster.ac.uk>
2022-05-31 15:09:13 +0000

    Change for ready to the final project

--- script.js
+++ script.js
@@ -13,12 +13,16 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     //Update front end result element
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML=
+    "If you deposit <span style='background-color: yellow;'>"+principal+
+    "</span>,\<br\>at an interest rate of <span style='background-color: yellow;'>"+rate+
+    "%</span>\<br\>You will receive an amount of <span style='background-color: yellow;'>"+interest+
+    "</span>,\<br\>in the year <span style='background-color: yellow;'>"+year+"</span>\<br\>";
 }
 
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
         
